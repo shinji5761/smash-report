94ebd54f39afddb69075d3442707caec6b78c5c5
shinji5761 <shinji.tec.5761@gmail.com>
2020-12-27 13:00:47 +0900

    ui,db_master_setting

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,17 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import fs = require( 'fs' );
+
+const httpsOptions = {
+  key: fs.readFileSync('/etc/letsencrypt/live/smash-report.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/smash-report.com/cert.pem'),
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger : [ 'error', 'warn' , 'debug', 'log' ]
+    logger : [ 'error', 'warn' , 'debug', 'log' ],
+    httpsOptions
   });
   const configService = app.get(ConfigService);
   app.enableCors({
