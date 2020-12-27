738ed18115a08c732f93fba1f3e8e0007c18ba5d
shinji5761 <shinji.tec.5761@gmail.com>
2020-12-27 12:59:56 +0900

    config設定

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -10,10 +10,15 @@ import { UserModule } from './users/users.module';
 // Report API
 import { Report, SummaryReport } from './report/report';
 import { ReportModule } from './report/report.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'db',
@@ -21,6 +26,11 @@ import { ReportModule } from './report/report.module';
       username: 'postgres',
       password: 'postgres',
       database: 'smash_report',
+      // host: 'smash-report-db.clgb2sjfkh0r.ap-northeast-1.rds.amazonaws.com',
+      // port: 5432,
+      // username: 'smash_report',
+      // password: 'Infinite33',
+      // database: 'smash_report',
       entities: [ Users, Report, SummaryReport ],
       synchronize: true,
     }),
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,10 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger : [ 'error', 'warn' , 'debug', 'log' ]
   });
+  const configService = app.get(ConfigService);
   app.enableCors({
     origin: "*",
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept'
   });
-  await app.listen(3000);
+  await app.listen(configService.get( 'PORT' ) || 3000 );
 }
 bootstrap();
